e87dc33d876190d7d88cc1277106d6ca4dd3dc9b
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-08-19 15:43:12 +0300

    switch back to sqlite and fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:ecuSinner/pin_board.git'
 
 set :passenger_restart_with_touch, true
 
-set :deploy_to, '/home/deivis/notes'
+set :deploy_to, '/home/deivis/pin_board'
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
